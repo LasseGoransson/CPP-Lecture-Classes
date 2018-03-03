fd7c9040531167198be824e044b5e3117ea1032c
Lasse Nørregaard Gøransson <lagoe16@student.sdu.dk>
2018-03-03 11:59:07 +0100

    First start on driver class

--- Matador/Matador/Driver.h
+++ Matador/Matador/Driver.h
@@ -1,9 +1,20 @@
 #pragma once
+#include "Field.h"
+#include "Player.h"
+using namespace std;
+
 class Driver
 {
 public:
 	Driver();
 
+	void tick();
+
 	~Driver();
+
+
+private:
+	Field fieldArray [40];
 };
 
+	
